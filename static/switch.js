006a497dd2f9b278529b44c731766a81aca560b9
w3rr0 <konradmateja65@gmail.com>
2025-01-16 00:46:33 +0100

    add/change background image based on selected theme

--- static/switch.js
+++ static/switch.js
@@ -6,6 +6,16 @@ toggleSwitch.addEventListener("change", () => {
   const mode = toggleSwitch.checked ? "Dark" : "Light";
   statusText.textContent = mode;
 
+  // Change the theme
+  if (mode === "Dark") {
+    document.body.style.backgroundImage = "url('/static/background_night.jpg')";
+    // document.body.style.color = "#fff";
+    // powyżej zmiana tekstu na biały
+  } else {
+    document.body.style.backgroundImage = "url('/static/background.jpg')";
+    document.body.style.color = "#000";
+  }
+
   // Send the mode to the server
   fetch("/change-theme", {
     method: "POST",
